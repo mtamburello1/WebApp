9784c657295143874f5d50419a8eec91c9ecb050
Martina Tamburello <m.tamburello1@studenti.unisa.it>
2020-11-20 01:20:35 +0100

    Creato Bianco e Nero

---
+++ Pages/Bianco e Nero/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Bianco_e_Nero
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
