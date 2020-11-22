02d1aa54ba91f5f8411b89d5fca2503bdb01fc13
Martina Tamburello <m.tamburello1@studenti.unisa.it>
2020-11-22 23:05:23 +0100

    Aggiunta pagina Raccolta

---
+++ wwwroot/js/BiancoENeroJs/ChangePage.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function ($) {
+            $("#right").on('click', (function (e) {
+                document.getElementById("left").removeAttribute("disabled");
+                var previous_value = document.getElementById("hidden").getAttribute("value");
+                var selected_value = parseInt(previous_value) + 1;
+                document.getElementById("hidden").setAttribute("value", selected_value);
+                document.getElementById("par" + selected_value).style.display = "table-cell";
+                document.getElementById("par" + previous_value).style.display = "none";
+
+                if (selected_value >= 3) {
+                    document.getElementById("right").setAttribute("disabled", true);
+                }
+
+                return;
+
+            }));
+
+        $("#left").on('click', (function (e) {
+
+            document.getElementById("right").removeAttribute("disabled");
+            var previous_value = document.getElementById("hidden").getAttribute("value");
+            var selected_value = parseInt(previous_value) - 1;
+            document.getElementById("hidden").setAttribute("value", selected_value);
+            document.getElementById("par" + selected_value).style.display = "table-cell";
+            document.getElementById("par" + previous_value).style.display = "none";
+
+            if (selected_value <= 1) {
+            document.getElementById("left").setAttribute("disabled", true);
+            }
+
+            return;
+
+        }));
+
+});
