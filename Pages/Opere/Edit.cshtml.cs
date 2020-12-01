b978919823c7a4414f3e21a109cf9dc5a0f32b1e
Rosario Di Palma <rosariodipalma@outlook.com>
2020-12-01 19:36:11 +0100

    Modificati gli Edit di Autore Opere e Personaggi per i campi inerenti all'aggiunta dei file

--- Pages/Opere/Edit.cshtml.cs
+++ Pages/Opere/Edit.cshtml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using WebApp.Data;
 using WebApp.Models;
 
@@ -38,12 +41,15 @@ namespace WebApp.Pages.Opere
                 return NotFound();
             }
            ViewData["AutoreId"] = new SelectList(_context.Autore, "AutoreId", "NomeAutore");
+
+            HttpContext.Session.SetString("videoCorrente", this.Opera.VideoOpera);
+            HttpContext.Session.SetString("fotoCorrente", this.Opera.FotoOpera);
             return Page();
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostUploadAsync(IFormFile fotoOpera, IFormFile videoOpera)
         {
             if (!ModelState.IsValid)
             {
@@ -51,6 +57,77 @@ namespace WebApp.Pages.Opere
             }
 
             _context.Attach(Opera).State = EntityState.Modified;
+
+            if (fotoOpera != null)
+            {
+                if (fotoOpera.Length > 0)
+                {
+                    //Prende il nome del file
+                    var fileName = Path.GetFileName(fotoOpera.FileName);
+
+                    //Assegna un nome univoco per il file
+                    var myUniqueFileName = Convert.ToString(Guid.NewGuid());
+
+                    //Prende l'estensione del file
+                    var fileExtension = Path.GetExtension(fileName);
+
+                    // Concatena file e estensione
+                    var newFileName = String.Concat(myUniqueFileName, fileExtension);
+
+                    // Combina il file col path
+                    var filepath = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img")).Root + $@"\{newFileName}";
+
+                    // Crea il path da salvare nel DB
+                    this.Opera.FotoOpera = newFileName;
+
+                    using (FileStream fs = System.IO.File.Create(filepath))
+                    {
+                        fotoOpera.CopyTo(fs);
+                        fs.Flush();
+                    }
+                }
+            }
+
+            if (videoOpera != null)
+            {
+                if (videoOpera.Length > 0)
+                {
+                    //Prende il nome del file
+                    var fileName = Path.GetFileName(videoOpera.FileName);
+
+                    //Assegna un nome univoco per il file
+                    var myUniqueFileName = Convert.ToString(Guid.NewGuid());
+
+                    //Prende l'estensione del file
+                    var fileExtension = Path.GetExtension(fileName);
+
+                    // Concatena file e estensione
+                    var newFileName = String.Concat(myUniqueFileName, fileExtension);
+
+                    // Combina il file col path
+                    var filepath = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img", "VideoOpere")).Root + $@"\{newFileName}";
+
+                    // Crea il path da salvare nel DB
+                    this.Opera.VideoOpera = newFileName;
+
+                    using (FileStream fs = System.IO.File.Create(filepath))
+                    {
+                        videoOpera.CopyTo(fs);
+                        fs.Flush();
+                    }
+                }
+            }
+
+            if (this.Opera.VideoOpera == null)
+            {
+                this.Opera.VideoOpera = HttpContext.Session.GetString("videoCorrente");
+            }
+
+            if (this.Opera.FotoOpera == null)
+            {
+                this.Opera.FotoOpera = HttpContext.Session.GetString("fotoCorrente");
+            }
+
 
             try
             {
