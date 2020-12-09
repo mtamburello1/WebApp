1a553a5dd2fa5c5687a3dad7ca36571347fb16fd
Martina Tamburello <m.tamburello1@studenti.unisa.it>
2020-12-09 10:30:03 +0100

    script cambio paragrafo I vini del territorio

---
+++ wwwroot/js/BiancoENeroJs/ChangePage - IViniDelTerritorio.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function ($) {
+            $("#right").on('click', (function () {
+                document.getElementById("left").removeAttribute("disabled");
+                var previous_value = document.getElementById("hidden").getAttribute("value");
+                var selected_value = parseInt(previous_value) + 1;
+                if (selected_value >= 1)
+                {
+                    document.getElementById("right").setAttribute("disabled", true);
+                }
+                document.getElementById("hidden").setAttribute("value", selected_value);
+                document.getElementById("par" + selected_value).style.display = "table-cell";
+                document.getElementById("par" + previous_value).style.display = "none";
+
+
+                return;
+
+            }));
+
+            $("#left").on('click', (function (e) {
+
+                document.getElementById("right").removeAttribute("disabled");
+                var previous_value = document.getElementById("hidden").getAttribute("value");
+                var selected_value = parseInt(previous_value) - 1;
+                if (selected_value <= 0)
+                {
+                    document.getElementById("left").setAttribute("disabled", true);
+                }
+                document.getElementById("hidden").setAttribute("value", selected_value);
+                document.getElementById("par" + selected_value).style.display = "table-cell";
+                document.getElementById("par" + previous_value).style.display = "none";
+                return;
+
+            }));
+
+});
